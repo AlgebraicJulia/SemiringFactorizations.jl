239ebdb7ef7124528c856003b47561ab4339f477
Documenter.jl <documenter@juliadocs.github.io>
2025-12-23 20:20:02 +0000

    build based on c5f4534

--- dev/examples/scheduling.jl
+++ dev/examples/scheduling.jl
@@ -1,128 +1,135 @@
-# # Examples: Scheduling with SemiringFactorizations.jl
-# This literate example demonstrates how to use **SemiringFactorizations.jl**
-# to solve scheduling problems.
+# # PERT Networks
 #
-# We demonstrate both max-plus and min-plus formulations.
-
+# A PERT network encodes a project as a vertex-weighted graph ``G = (V, E, w)``.
+#
+# - **vertices** ``i \in V`` represent tasks.
+# - **edges** ``(i, j) \in E`` represent precedence constraints: task ``i`` must finish before task ``j`` can start.
+# - **vertex weights** ``w_i \in [0, \infty]`` represent durations: task ``i`` takes ``w_i`` days to complete. 
+#
+# The figure below shows a PERT network with eight tasks and ten precedence constraints. 
+#
+# ![PERT network](assets/scheduling.svg)
+#
+# We can represent the network as a matrix ``A: V \times V \to [-\infty, \infty]``, where
+# ```math
+# A_{ij} = \begin{cases}
+#     w_i     &\text{if } (i, j) \in    E \\
+#     -\infty &\text{if } (i, j) \notin E
+# \end{cases}
+# ```
+# for all pairs of tasks ``(i, j) \in V \times V``.
 using SemiringFactorizations, SparseArrays
-const Max64 = MaxPlus{Float64}
-const Min64 = MinPlus{Float64}
 
-# ## Max Completion
+## max-plus semiring
+const MP = MaxPlus{Float64}
 
-# This system represents a list of tasks that need to be completed and we want to know how long the longest sequence takes to
-# complete. The solution to this system gives us the amount of time needed to complete all tasks. We can encode sequential tasks,
-# delayed starts, and lag between tasks.
+## durations
+w = MP[3, 2, 4, 3, 5, 2, 6, 4]
+
+## precedence constraints
+##    1  1  2  2  3  4  4  5  6  7
+##    ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
+##    3  6  3  4  5  7  5  6  7  8
+I = [1, 1, 2, 2, 3, 4, 4, 5, 6, 7]
+J = [3, 6, 3, 4, 5, 7, 5, 6, 7, 8]
+
+A = sparse(I, J, w[I], 8, 8)
+
+# The Kleene star of ``A`` is a matrix ``A^*: V \times V \to [-\infty, \infty]``
+# whose entry
+# ```math
+# A^*_{ij} \in [-\infty, \infty]
+# ```
+# is equal to the shortest possible interval between the start of ``i`` and the start of ``j``. In
+# particular, if ``j`` can precede ``i``, then ``A^*_{ij} = -\infty``.
+# We can compute ``A^*`` explicitly using the function `star`.
+star(A)
+
+# Alternatively, we can compute a factorized representation of ``A^*`` using the function `slu`.
+# This approach is generally faster than the previous one.
+K = slu(A)
+
+# ## Scheduling
 #
-# For this example we have 8 tasks. Task 1 starts at time = 0 since it has no predecessors, Task 2 starts at time = 1 (its release time),
-# Task 3 starts at time = 4 (after Task 1 finishes at time = 3 + 1 lag, Task 2 finishes at time = 1 + 2), and so on.
-# 
-# This scenario is graphed below
+# We will also distinguish two sets of tasks.
+#   - **start** tasks: ``S \subseteq V``
+#   - **final** tasks: ``F \subseteq V``
+# Our goal is to complete the tasks in ``F``, starting with the tasks in ``S``.
+S = [1, 2]; F = [8];
+
+# We can represent ``S`` as a vector ``s: V \to [-\infty, \infty]``, where
+# ```math
+# s_i = \begin{cases}
+#     0       &\text{if } i \in    S \\
+#     -\infty &\text{if } i \notin S
+# \end{cases}
+# ```
+s = zeros(MP, 8); s[S] .= 0
+s
+
+# We can represent ``F`` as a vector ``f: V \to [-\infty, \infty]``, where
+# ```math
+# f_i = \begin{cases}
+#     w_i     &\text{if } i \in    F \\
+#     -\infty &\text{if } i \notin F
+# \end{cases}
+# ```
+f = zeros(MP, 8); f[F] .= w[F]
+f
+
+# ## Earliest Start and Finish Times
 #
-# ![Task graph](assets/scheduling.svg)
+# For all tasks ``i \in V``, the *earliest start time*
+# ```math
+# s^e_i \in [0, \infty]
+# ```
+# is the earliest time that ``i`` can begin.
+se = transpose(transpose(s) * K)
+
+# The *earliest finish time*
+# ```math
+# f^e_i \in [0, \infty]
+# ```
+# is the earliest time that ``i`` can be completed.
+fe = se .* w
+
+# ## Makespan
 #
-# We first set duration values for each task. For example we have set task 3 to take 4 units of time.
+# The *makespan* is the earliest
+# that the project can be completed. If the project takes
+# longer than this time, then it is *behind schedule*.
+c = transpose(se) * f
 
-d = Max64[3, 2, 4, 3, 5, 2, 6, 4]
-
-# Our initial matrix A will be a Max Plus matrix with float values and -Inf. We set this matrix to be n x n.
-# We now fill the initial Max Plus matrix with the constraints, valid ways to move throught the tasks and the lag for specific paths.
-# constraints (finish→start) with lags ℓ_ij
-# A[j,i] = d[i] + ℓ_ij if i → j, else -Inf
-
-l = Max64[1, 0, 2, 1, 0, 2, 1, 0, 3, 2]
-I = [3, 3, 4, 5, 5, 6, 7, 8, 7, 6]
-J = [1, 2, 2, 3, 4, 5, 6, 7, 3, 1]
-A = sparse(I, J, d[J] .* l, 8, 8)
-
-# Release Times: these are the time it takes to start a given task meaning that for example task 1 can start at time = 0
-# or no startup cost, while task 2 can start at time = 1 or 1 unit of startup cost. If a release value is set to -Inf then
-# we are not able to start from that task, so in this case we can only start from tasks 1 or 2.
-b = Max64[0, 1, -Inf, -Inf, -Inf, -Inf, -Inf, -Inf]
-
-# Kleene star
-S = slu(A)
-
-# Fixed point solution for x satisfies x = A ⊗ x ⊕ b
-x = S * b
-
-# Finish times y = x + d
-y = x .* d
-
-# Max completion
-# This represents the maximum cost it takes to complete the last task, which is calculated just by finding the longest path from
-# any avaliable starting task to the final task
-# In this case the true solution is the path 1 -> 3 -> 5 -> 6 -> 7 -> 8, 
-# total cost 3(1) + 1(1->3) + 4(3) + 1(3->5) + 5(5) + 2(5->6) + 2(6) + 1(6->7) + 6(7) + 0(7->8) + 4(8) = 29
-# It is worth noting that this vector gives us the maximum cost to complete any task given the starting tasks so in cases where 
-# the tasks are not semi sequential we would still get the highest cost for completion. 
-
-# Max-plus matrix A
-A
-
-# Kleene star A*
-Matrix(S)
-
-# Earliest start times x
-x
-
-# Finish times y = x + d
-y
-
-# Completion time
-Cmax = sum(y)
-
-# ## Now Min cost 
+# ## Latest Start and Finish Times
 #
-# We now consider a min cost problem where we want to have the lowest cost to reach and complete the final task.
-# We can constrain our starting location and ways to traverse through the task list to reach the solution.
-# This is a version of the shortest path problem.
+# For all tasks ``i \in V``, the *latest start time*
+# ```math
+#     s^l_i \in [0, \infty]
+# ```
+# is the latest time that ``i`` can begin without
+# the project falling behind schedule.
+sl = transpose(c / (K * f))
+
+# The *latest finish time*
+# ```math
+#     f^l_i \in [0, \infty]
+# ```
+# is the latest time that ``i`` can finish without
+# the project falling behind schedule.
+fl = sl .* w
+
+# ## Slack and Critical Tasks
 #
-# We first set duration values for each task. For example we have set task 3 to take 4 units of time.
+# For all tasks ``i \in V``, the difference
+# ```math
+#     s^l_i - s^e_i \in [0, \infty]
+# ```
+# is called the *slack* at ``i``. It encodes how long
+# ``i`` can be delayed before the project falls behind
+# schedule.
+slack = sl ./ se
 
-d = Min64[3, 2, 4, 3, 5, 2, 6, 4]
-
-# Our initial matrix A will be a Min Plus matrix with float values and Inf. We set this matrix to be n x n.
-# We now fill the initial Min Plus matrix with the constraints, valid ways to move throught the tasks and the lag for specific paths.
-# constraints (finish→start) with lags ℓ_ij
-# A[j,i] = d[i] + ℓ_ij if i → j, else -Inf
-
-l = Min64[1, 0, 2, 1, 0, 2, 1, 0, 3, 2]
-I = [3, 3, 4, 5, 5, 6, 7, 8, 7, 6]
-J = [1, 2, 2, 3, 4, 5, 6, 7, 3, 1]
-A = sparse(I, J, d[J] .* l, 8, 8)
-
-# Release Times: these are the time it takes to start a given task meaning that for example task 1 can start at time = 0
-# or no startup cost, while task 2 can start at time = 1 or 1 unit of startup cost. If a release value is set to Inf then
-# we are not able to start from that task, so in this case we can only start from tasks 1 or 2.
-b = Min64[0, 1, Inf, Inf, Inf, Inf, Inf, Inf]
-
-# Kleene star
-S = slu(A)
-
-# Fixed point solution for x satisfies x = A ⊗ x ⊕ b
-x = S * b
-
-# Finish times y = x + d
-y = x .* d
-
-# Min completion
-# This represents the minimum cost it takes to complete task 8 starting at either task 1 or 2.
-# In this case the true solution is the path 1 -> 6 -> 7 -> 8, total cost 3(1) + 2(1->6) + 2(6) + 1(6->7) + 6(7) + 0(7->8) + 4(8) = 18
-# It is worth noting that this vector gives us the minimum cost to complete any task given the starting tasks so in cases where 
-# the tasks are not semi sequential we would still get the least cost for completion. 
-
-# Min-plus matrix A
-A
-
-# Kleene star A*
-Matrix(S)
-
-# Minimum cumulative costs x"
-x
-
-# Finish costs y = x + d
-y
-
-# Minimum Cost
-Cmin = reduce(∧, y)
+# A task ``i \in V`` is called *critical* if its slack is zero.
+# Critical tasks cannot be delayed without delaying the whole
+# project.
+critical = findall(isone, slack) 
